0664a18a869d24b5763b08bd7b45ddaa2d95744e
Ayush Yadav <77686324+AyushUnleashed@users.noreply.github.com>
2022-10-02 03:30:42 +0530

    Time: 78 ms (14.58%), Space: 32.6 MB (81.29%) - LeetHub

---
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.cpp
@@ -0,0 +1,87 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include<limits.h>
+class Solution {
+public:
+    
+    int maximumOfBST(TreeNode* root){
+        
+        if(root==NULL){
+            return INT_MIN;
+        }
+        
+        if(root->right==NULL){
+            return root->val;
+        }else{
+            return maximumOfBST(root->right);
+        }
+        
+    }
+    
+    
+    
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        // 1 root==NULL return NULL
+        //2  key< root , LeftSide , modify left and reattach, return root
+        //3  root<key , right side , modify right and reattach, return root
+        
+        //4 root == key , need to delete  current root
+        // 4a root->left==NULL && root->right == NULL  return NULL
+        // 4b root->left==NULL || root->right == NULL  return the side which is not null
+        // 4c root->left!=NULL && root->right!= NULL
+        /* we find a replacement for root
+         maximum of leftSubtree or minimum of right subtree can replace the root
+        
+        we find maximum of leftsubtree, 
+        replace it with root, 
+        delete the max for leftsubtree
+        
+        return the new root
+        */
+        
+        
+        if(root==NULL) return NULL; // if key doesn't exist this will eventually occur
+        
+        if(key<root->val){
+            //leftside
+            root->left = deleteNode(root->left,key);
+            return root;
+        }else if(root->val<key){
+            //right side
+            root->right = deleteNode(root->right,key);
+            return root;
+        }else if(root->val==key){
+            //4
+            
+            if(root->left==NULL && root->right==NULL){
+                return NULL;
+            }else if(root->left==NULL && root->right!=NULL){
+                return root->right;
+            }else if(root->left!=NULL && root->right==NULL){
+                return root->left;
+            }else if(root->left!=NULL && root->right!=NULL){
+                
+                //last case
+                int leftMax = maximumOfBST(root->left);
+                root->val = leftMax;
+                root->left = deleteNode(root->left,leftMax);
+                
+                return root;
+                
+            }
+            
+        }
+        
+        cout<<"This should not happen";
+        return NULL;
+    }
+};
