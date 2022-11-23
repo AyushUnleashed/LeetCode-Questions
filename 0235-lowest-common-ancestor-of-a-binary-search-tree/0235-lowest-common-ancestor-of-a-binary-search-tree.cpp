ac099857112c690d19e95468de45cda02ccc2655
Ayush Yadav <77686324+AyushUnleashed@users.noreply.github.com>
2022-11-23 19:51:28 +0530

    Time: 61 ms (57.25%), Space: 23.3 MB (19.53%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -12,56 +12,34 @@ class Solution {
 public:
 
     
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q){
-        if(root==NULL) return NULL;
-        
-        vector<TreeNode*> pNodes =getPath(root,p->val);
-        vector<TreeNode*> qNodes =getPath(root,q->val);
-        
-        int i=0; TreeNode* lcaNode=NULL;
-        while(i<pNodes.size() && i<qNodes.size()&& (pNodes[i]->val==qNodes[i]->val)){
-            lcaNode=pNodes[i];
-            i++;
-        }
-        
-        return lcaNode;
-            
-    }
-    
-    
-    //returns true if node is found else return false;
-    bool helper(TreeNode* root,vector<TreeNode*> &finalAns,int B){
 
+
+
+
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
         if(root==NULL){
-            return false;
+            return NULL;
         }
-
-        finalAns.push_back(root);
-
-        if(root->val==B){
-            return true;
+        
+        if(root->val==p->val || root->val==q->val){
+            return root;
         }
-
-        bool leftAns = helper(root->left,finalAns,B);
-        bool rightAns = helper(root->right,finalAns,B);
-
-        if(leftAns || rightAns){
-
-            //finalAns.push_back(root->val);
-            return true;
+        
+        TreeNode* leftAns = lowestCommonAncestor(root->left,p,q);
+        TreeNode* rightAns = lowestCommonAncestor(root->right,p,q);
+        
+        if(leftAns==NULL && rightAns==NULL){
+            return NULL;
+        }else  if(leftAns!=NULL && rightAns!=NULL){
+            return root;
+        }else  if(leftAns!=NULL && rightAns==NULL){
+            return leftAns;
+        }else  if(leftAns==NULL && rightAns!=NULL){
+            return rightAns;
         }
-
-        // both left & right are false
-        finalAns.pop_back();
-        return false;
-    }
-    
-    vector<TreeNode*> getPath(TreeNode* A, int B) {
-        vector<TreeNode*> finalAns;
-        if(A==NULL) return finalAns;
-
-        bool ans = helper(A,finalAns,B);
-        return finalAns;
+        
+        return NULL;
     }
     
 };
@@ -90,33 +68,3 @@ public:
 
 
 
-
-
-
-
-
-//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        
-//         if(root==NULL){
-//             return NULL;
-//         }
-        
-//         if(root->val==p->val || root->val==q->val){
-//             return root;
-//         }
-        
-//         TreeNode* leftAns = lowestCommonAncestor(root->left,p,q);
-//         TreeNode* rightAns = lowestCommonAncestor(root->right,p,q);
-        
-//         if(leftAns==NULL && rightAns==NULL){
-//             return NULL;
-//         }else  if(leftAns!=NULL && rightAns!=NULL){
-//             return root;
-//         }else  if(leftAns!=NULL && rightAns==NULL){
-//             return leftAns;
-//         }else  if(leftAns==NULL && rightAns!=NULL){
-//             return rightAns;
-//         }
-        
-//         return NULL;
-//     }
