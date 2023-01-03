1dc8ca15a0aed022b11a81ba35b76c5dd89f8f14
Ayush Yadav <77686324+AyushUnleashed@users.noreply.github.com>
2023-01-03 15:16:46 +0530

    Time: 47 ms (5.46%), Space: 17.6 MB (5.75%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    
+    vector<int> createInorder(TreeNode* root){
+        vector<int> ans;
+        if(root==NULL){
+            return ans;
+        }
+        //left
+        ans = createInorder(root->left);
+        //root
+        ans.push_back(root->val);
+        //right
+        vector<int> rightAns = createInorder(root->right);
+        ans.insert(ans.end(),rightAns.begin(),rightAns.end());
+        return ans;
+    }
+    
+    TreeNode* modifyTraversal(TreeNode* root, unordered_map<int,int> hashMap){
+        //inorder traversal
+        //left
+        if(root==NULL) {
+            return NULL;
+        }
+        TreeNode* leftSubTree = modifyTraversal(root->left,hashMap);
+        TreeNode* newRoot = new TreeNode(hashMap[root->val]);
+        TreeNode* rightSubTree = modifyTraversal(root->right,hashMap);        
+        newRoot->left = leftSubTree;
+        newRoot->right = rightSubTree;
+        return newRoot;
+    }
+        TreeNode* bstToGst(TreeNode* root) {
+        
+        vector<int> inorder = createInorder(root);   
+        // modify inorder
+        vector<int> modifiedInorder = inorder;
+        
+        int len= modifiedInorder.size();
+        int runningSum=0;
+        unordered_map<int,int> hashMap;
+        
+        for(int i=len-1;i>=0;i--){
+            runningSum+=inorder[i];
+            modifiedInorder[i]=runningSum;
+            hashMap[inorder[i]]=modifiedInorder[i];
+        }
+        
+       return modifyTraversal(root,hashMap);
+    }
+    
+};
