92355479f5265daf6bc865adfd2170f24dd13c8a
Ayush Yadav <77686324+AyushUnleashed@users.noreply.github.com>
2023-01-22 18:48:38 +0530

    Time: 131 ms (63.25%), Space: 47.3 MB (32.29%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        // build mean heap of K elements top element is ans
+        priority_queue<int,vector<int>,greater<int>> pq;
+        for(int i=0;i<k;i++){
+            pq.push(nums[i]);
+        }
+        
+        for(int i=k;i<nums.size();i++){
+            
+            if(pq.top()<nums[i]){
+                pq.pop();
+                pq.push(nums[i]);
+            }
+        }
+        
+        return pq.top();
+    }
+};
