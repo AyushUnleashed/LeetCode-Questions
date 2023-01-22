0d540488273fa50ea457cafdf75a0b823a71b1d7
Ayush Yadav <77686324+AyushUnleashed@users.noreply.github.com>
2023-01-22 21:00:23 +0530

    Time: 29 ms (67.13%), Space: 13.7 MB (29.60%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,39 +10,89 @@
  */
 class Solution {
 public:
+
+    class Node{
+        public:
+        ListNode* node;
+        int i;
+        
+        Node(ListNode* node,int i){
+            this->node=node;
+            this->i=i;
+        }
+    };
+    
+    class Compare{
+        public:
+        bool operator()(Node a,Node b){
+            if(a.node->val > b.node->val){
+                return true;
+            }else{
+                return false;
+            }
+        }
+    };
+
+        
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         
-        ListNode* finalHead = NULL;
-        ListNode* finalTail=NULL;
-        ListNode* temp = finalHead;
-        
-        priority_queue<int, vector<int>, greater<int>> pq;
-        
-        for(auto& listPtr: lists){
-            ListNode* head = listPtr;
-            
-            while(head!=NULL){
-                pq.push(head->val);
-                head = head->next;
-            }
-            
+        int k=lists.size();
+        if(k==0){
+            return NULL;
         }
+        
+        ListNode* mergedHead=NULL;
+        ListNode* mergedTail=NULL;
+        
+        priority_queue<Node,vector<Node>, Compare> pq;
+      //build heap from first elements of all lists
+        
+        vector<ListNode*> ptr(k);
+        
+        for(int i=0;i<k;i++){
+            ptr[i]=lists[i];
+            if(ptr[i]!=NULL)
+            {
+                pq.push(Node(ptr[i],i));
+            }
+        }
+        
+        if(pq.empty()){
+            return NULL;
+        }
+        
+                
+        // add first element to final merged list
+        mergedHead = pq.top().node;
+        mergedTail = pq.top().node;
+    
+        int index = pq.top().i;
+        pq.pop();
+        
+        ptr[index]=ptr[index]->next;
+
+        if(ptr[index]!=NULL){
+           pq.push(Node(ptr[index],index));
+        }
+
         
         while(!pq.empty()){
+           
+            int index = pq.top().i;
+            ListNode* temp = pq.top().node;
+            pq.pop();
+        
+            mergedTail->next = temp;
+            mergedTail=mergedTail->next;
             
-            ListNode* newNode = new ListNode(pq.top());
-            if(finalHead==NULL && finalTail==NULL){       
-                finalHead = newNode;
-                finalTail = newNode;
-                
-            }else{
-                finalTail->next= newNode;
-                finalTail=finalTail->next;
-               
+            ptr[index]=ptr[index]->next;
+            
+            if(ptr[index]!=NULL){
+                pq.push(Node(ptr[index],index));
             }
-             pq.pop();
         }
         
-        return finalHead;
+        return mergedHead;
+        
     }
 };
