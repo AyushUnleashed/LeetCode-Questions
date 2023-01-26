1aeac027c72674c24d6a75c272aae99ea2704a0f
Ayush Yadav <77686324+AyushUnleashed@users.noreply.github.com>
2023-01-26 21:48:21 +0530

    Time: 210 ms (31.62%), Space: 47.2 MB (46.66%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -4,23 +4,35 @@ using namespace std;
 
 class Solution {
 public:
+    
     int findKthLargest(vector<int>& nums, int k) {
-        // build mean heap of K elements top element is ans
-        priority_queue<int,vector<int>,greater<int>> pq;
-        for(int i=0;i<k;i++){
-            pq.push(nums[i]);
-        }
-        
-        for(int i=k;i<nums.size();i++){
-            
-            if(pq.top()<nums[i]){
+       priority_queue<int,vector<int>,greater<int>> pq;
+        for(auto &x:nums){
+            pq.push(x);
+            if(pq.size()>k){
                 pq.pop();
-                pq.push(nums[i]);
             }
         }
-        
         return pq.top();
+        
     }
+//     int findKthLargest(vector<int>& nums, int k) {
+//         // build mean heap of K elements top element is ans
+//         priority_queue<int,vector<int>,greater<int>> pq;
+//         for(int i=0;i<k;i++){
+//             pq.push(nums[i]);
+//         }
+        
+//         for(int i=k;i<nums.size();i++){
+            
+//             if(pq.top()<nums[i]){
+//                 pq.pop();
+//                 pq.push(nums[i]);
+//             }
+//         }
+        
+//         return pq.top();
+//     }
     
 //     int findKthLargest(vector<int>& nums, int k){
 //         priority_queue<int> pq;
