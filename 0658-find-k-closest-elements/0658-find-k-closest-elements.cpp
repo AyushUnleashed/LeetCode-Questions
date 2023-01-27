6355b4b680da7f539ce46f8de2656c023b7df7cd
Ayush Yadav <77686324+AyushUnleashed@users.noreply.github.com>
2023-01-28 03:03:20 +0530

    Time: 148 ms (14.75%), Space: 33.9 MB (38.10%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,37 +1,33 @@
 //first is diff
-//second is index
+//second is val
 
 class Solution {
 public:
     
 
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        vector<pair<int,int>> diffV;
+    
         priority_queue< pair<int,int>> pq; //Max Heap, so max diff on top gets popped
         
         for(int i=0;i<arr.size();i++){
-            int diff = abs(arr[i]-x);
-            diffV.push_back({diff,arr[i]});
-        }
-        
-        vector<int> ans;
-        for(int i=0;i<diffV.size();i++){
             
-            pq.push(diffV[i]);
+            int diff = abs(arr[i]-x);
+            pq.push({diff,arr[i]});
             
             if(pq.size()>k){
                 pq.pop();
             }
         }
-    
-    
+        
+        vector<int> ans;
         while(!pq.empty()){
             int val=pq.top().second;
             ans.push_back(val);
             pq.pop();
         }
-        sort(ans.begin(),ans.end());
+        sort(ans.begin(),ans.end()); // kLog(k)
         return ans;
         
+        //kLog(n) - overall
     }
 };
