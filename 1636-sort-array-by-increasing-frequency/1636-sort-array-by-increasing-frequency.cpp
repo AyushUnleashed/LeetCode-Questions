e0507869b1e0f3f3acbb71dc3246164bbe03d9e2
Ayush Yadav <77686324+AyushUnleashed@users.noreply.github.com>
2023-01-28 20:23:55 +0530

    Time: 12 ms (53.12%), Space: 11.5 MB (42.52%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -1,3 +1,5 @@
+// PQ => reverse of normal in comparator
+
 class Solution {
 public:
     
@@ -7,17 +9,29 @@ public:
     class Compare{
         public:
         bool operator()(pair<int,int> a,pair<int,int> b){
-            if(a.first<b.first){
-                //swap
-                return false;
+            // Jo condition chahiye return true;
+            // a, left .. b, right
+            if(a.first>b.first){
+                return true;
             }else if(a.first==b.first){
-                if(a.second>b.second){
-                    return false;
-                }else return true;
-            }else return true;
+                if(a.second<b.second){
+                    return true;
+                }else return false;
+            }else return false;
         }
-        
     };
+    
+    // static bool comparator(pair<int,int> a,pair<int,int> b){
+    //         // Jo condition chahiye return true;
+    //         // a, left .. b, right
+    //         if(a.first<b.first){
+    //             return true;
+    //         }else if(a.first==b.first){
+    //             if(a.second>b.second){
+    //                 return true;
+    //             }else return false;
+    //         }else return false;
+    // }
 
     
     vector<int> frequencySort(vector<int>& nums) {
@@ -43,6 +57,19 @@ public:
             pq.pop();
         }
         // pop elements & push them frequency times
+        
+//         vector<pair<int,int>> temp;
+//         for(auto i:mp) temp.push_back({i.second,i.first});
+//         sort(temp.begin(),temp.end(),comparator);
+        
+//         for(int i=0;i<temp.size();i++){
+//             int freq=temp[i].first;
+//             while(freq>0){
+//                ans.push_back(temp[i].second);
+//                 freq--;
+//             }
+//         }
+        
         return ans;
     }
 };
