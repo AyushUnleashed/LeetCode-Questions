927d00a3aa3c1b9c60c7ad5888d9231ec03fafed
Ayush Yadav <77686324+AyushUnleashed@users.noreply.github.com>
2023-01-28 19:08:45 +0530

    Time: 11 ms (62.96%), Space: 11.4 MB (42.52%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -3,16 +3,22 @@ public:
     
     //first -> freq 
     //second -> element
-    static bool comparator(pair<int,int> a,pair<int,int> b){
-        if(a.first>b.first){
-            //swap
-            return false;
-        }else if(a.first==b.first){
-            if(a.second<b.second){
+    
+    class Compare{
+        public:
+        bool operator()(pair<int,int> a,pair<int,int> b){
+            if(a.first<b.first){
+                //swap
                 return false;
+            }else if(a.first==b.first){
+                if(a.second>b.second){
+                    return false;
+                }else return true;
             }else return true;
-        }else return true;
-    }
+        }
+        
+    };
+
     
     vector<int> frequencySort(vector<int>& nums) {
         unordered_map<int,int> mp;
@@ -20,20 +26,23 @@ public:
         for(int i=0;i<nums.size();i++){
             mp[nums[i]]++;
         }
-        vector<pair<int,int>> temp;
-        for(auto i:mp) temp.push_back({i.second,i.first});
-        // push freq, element
         
-        //sort vector acc. to freq
-        sort(temp.begin(),temp.end(),comparator);
-        for(auto &v:temp){
-            int freq=v.first;
+        //get frequency of each element
+        priority_queue<pair<int,int>,vector<pair<int,int>>,Compare> pq;
+        
+        for(auto i:mp) pq.push({i.second,i.first});
+        // push freq, element to Heap
+        
+
+        while(!pq.empty()){
+            int freq=pq.top().first;
             while(freq>0){
-               ans.push_back(v.second);
+               ans.push_back(pq.top().second);
                 freq--;
             }
-            
+            pq.pop();
         }
+        // pop elements & push them frequency times
         return ans;
     }
 };
