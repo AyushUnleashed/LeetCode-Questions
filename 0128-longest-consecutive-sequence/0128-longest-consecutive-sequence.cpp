925dce66850d3d7b51edd827f146ebbb1343b9b6
Ayush Yadav <77686324+AyushUnleashed@users.noreply.github.com>
2023-02-03 21:21:26 +0530

    Time: 140 ms (78.27%), Space: 49.7 MB (60.13%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -2,27 +2,32 @@ class Solution {
 public:
     
      int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> hs;
+        unordered_map<int,bool> mp;
         int maxLen=0;
          
          for(int i=0;i<nums.size();i++){
-             hs.insert(nums[i]);
+             mp[nums[i]]=false;
          }
          //all of them are starting of sequence
+         //false means, it has not been iterated on.
+         // we use this boolean to avoid reiterating the while loop
          
          for(int i=0;i<nums.size();i++){
              
              // if smaller element doesn't exit
-             if(hs.find(nums[i]-1)==hs.end()){
+             if(mp.find(nums[i]-1)==mp.end()){
+                 
+                 if(mp[nums[i]]==true) continue;
                  
                  // no smaller element exist, it's smallest in map
                  //start counting upwards
                  
                  int len=0;
-                 while(hs.find(nums[i]+len)!=hs.end()){
+                 while(mp.find(nums[i]+len)!=mp.end()){
                      //if next element exist
                      len++; 
                  }
+                 mp[nums[i]]=true; // we have iterated on this element
                  maxLen=max(len,maxLen);
              }
          }
