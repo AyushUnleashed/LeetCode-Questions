ca1f32c29293873d94d2503e1bc660001e15d8f5
Ayush Yadav <77686324+AyushUnleashed@users.noreply.github.com>
2023-03-03 23:38:29 +0530

    Time: 0 ms (100.00%), Space: 6 MB (75.14%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -8,25 +8,12 @@ public:
     }
     
     int climbStairs(int n) {
-        if(n==1 || n==2){
+        if(n<=2){
             return n;
         }
-        int ans1,ans2;
-        if(dp[n-1]!=-1){
-           ans1 = dp[n-1]; 
-        }else{
-            ans1 = climbStairs(n-1);
-            dp[n-1]=ans1;
-        } 
-        
-        if(dp[n-2]!=-1){
-           ans2 = dp[n-2]; 
-        }else{
-            ans2 = climbStairs(n-2);
-            dp[n-2]=ans2;
-        } 
-
-        
-        return ans1+ans2;
+        if(dp[n]==-1){
+           dp[n] = climbStairs(n-1)+climbStairs(n-2);  
+        }
+        return dp[n];
     }
 };
