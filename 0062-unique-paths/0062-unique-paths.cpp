85c1e0d052103c8f50df0221c8d323177565ac61
Ayush Yadav <77686324+AyushUnleashed@users.noreply.github.com>
2023-03-21 17:04:44 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (33.21%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -2,30 +2,27 @@ class Solution {
     vector<vector<int>> dp;
 public:
     
-    int uniquePathsHelper(int x,int y,int m, int n){
+    int uniquePathsHelper(int i,int j,int m, int n){
         
-        if(x==n-1 || y==m-1) return 1;
-        if(x>=n || y>=m ) return 0;
+        if(i==m-1 || j==n-1) return 1;
+        if(i>=m || j>=n ) return 0;
         
-        if(dp[x][y]) return dp[x][y];
+        if(dp[i][j]) return dp[i][j];
         
-        int rightCall =uniquePathsHelper(x+1,y,m,n);
-        int bottomCall =uniquePathsHelper(x,y+1,m,n);
+        int rightCall =uniquePathsHelper(i,j+1,m,n);
+        int bottomCall =uniquePathsHelper(i+1,j,m,n);
         
         int totalPaths = rightCall + bottomCall;
-        dp[x][y]=totalPaths;
+        dp[i][j]=totalPaths;
         
         return totalPaths;
     }
     
     
     int uniquePaths(int m, int n) {
-        int x =0,y=0;
+        int i=0,j=0;
 
-        for(int i=0;i<n;i++){
-            vector<int> curr(m,0);
-            dp.push_back(curr);
-        }
-        return uniquePathsHelper(x,y,m,n);
+        dp.resize(m,vector<int> (n,0));
+        return uniquePathsHelper(i,j,m,n);
     }
 };
