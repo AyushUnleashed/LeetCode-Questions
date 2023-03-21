e57ab7caa79cf70e51e4465f0879aae2f4250dfc
Ayush Yadav <77686324+AyushUnleashed@users.noreply.github.com>
2023-03-21 12:17:14 +0530

    Time: 167 ms (77.38%), Space: 107.4 MB (97.44%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -5,6 +5,8 @@ public:
         long count=0;
         // count stores the no. of consecutive zeros
         long total=0; 
+        // no. of sub arrays = n*(n+1)/2 for n elements
+        
         for(int i=0;i<nums.size();i++){
             if(nums[i]==0){
                 count++;
